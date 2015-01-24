eeb72ff3c84c75608d57a70a26a6d1c8cd7816c8
Frank Cash <cfcash@mtu.edu>
2015-01-23 20:23:42 -0500

    Adds basic app.js component

---
+++ public/src/js/app.js
@@ -0,0 +1,18 @@
+angular.module('app', [])
+  .controller('Attendees', ['$scope', function($scope){
+    $scope.naomi = {
+      name: 'Naomi',
+      address: '1600 Amphiteather'
+    }
+
+    $scope.attendee = [];
+    $scope.attendee.push($scope.naomi);
+    $scope.attendee.push({name: 'Frank', address:'Foo123'})
+  }])
+  .directive('myAttendee', function(){
+    return{
+      restrict: 'E',
+      scope: true, // uses prototypical inheritence
+      templateUrl: 'public/src/templates/my-attendee.html'
+    }
+  })
