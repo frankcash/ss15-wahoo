a33c30900c64938709183924e50b3f63d958880c
jschimmoeller@schimmoeller.net <jschimmoeller@schimmoeller.net>
2015-01-25 10:56:10 -0500

    share page

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -5,8 +5,8 @@
         $routeProvider.
                 when('/home', {templateUrl: 'src/templates/home.html',   controller: 'IndexCtrl'}).
                 when('/eventList', {templateUrl: 'src/templates/all-events.html',   controller: 'EventsCtrl'}).
-                when('/event/:itemId', {templateUrl: 'src/templates/specific-event.html'}).
-                when('/eg/:eventId/:guestId', {templateUrl: 'src/templates/all-guests.html'}).
+                when('/event/:eventId', {templateUrl: 'src/templates/specific-event.html'}).
+                when('/share/:eventId/:guestId', {templateUrl: 'src/templates/share-memory.html'}).
                 when('/create', {templateUrl: 'src/templates/create-event.html',   controller: 'IndexCtrl'}).
                 when('/memory/:eventId/:guestId', {templateUrl: 'src/templates/my-memories.html',   controller: 'MemoriesCtrl'}).
                 when('/about', {templateUrl: 'src/templates/about.html',   controller: 'IndexCtrl'})
@@ -29,19 +29,112 @@
       }
   }])
 
-  app.controller('SpecificEventCtrl', ['$scope', '$location', 'eventsFactory', 'guestFactory','$route','$routeParams',
-    function($scope, $location, eventsFactory, guestFactory, $route, $routeParams){
+  app.controller('SpecificEventCtrl', ['$scope', '$location', 'eventsFactory', 'guestFactory','$route','$routeParams', 'memoriesFactory',
+    function($scope, $location, eventsFactory, guestFactory, $route, $routeParams, memoriesFactory){
       console.log('SpecificEventController');
       this.params = $routeParams;
-      $scope.thisEvent = this.params.itemId;
-      console.log("params",this.params.itemId);
+      $scope.thisEvent = this.params.eventId;
+      $scope.thisGuestId = this.params.guestId;
+      
+      console.log("params",this.params);
       $scope.eventInfo = eventsFactory.getEvent($scope.thisEvent);
       $scope.guestInfo = guestFactory.getGuests($scope.thisEvent);
       $scope.guestInfo.$loaded().then(function(data){
         console.log("GUEST INFO:", $scope.guestInfo, data);
       })
-      
+       $scope.errBack = function(e) {
+        console.log('Reeeejected!', e);
+      };     
       // this.event
+      $scope.hasMedia= function() {
+        console.log('herer');
+        var breturn= (window.navigator.getUserMedia || window.navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia);
+        
+        if (breturn) {
+          $scope.videoObj = { "video": true };
+          $scope.video =  document.getElementById("video");
+          $scope.canvas = document.getElementById("canvas")
+          $scope.context = $scope.canvas.getContext("2d")
+          $scope.myStream = null;
+          
+          if(window.navigator.getUserMedia) { // Standard
+            window.navigator.getUserMedia($scope.videoObj, function(stream) {
+                    $scope.video.src = stream;
+                    $scope.video.play();
+                    $scope.myStream = stream;
+            }, $scope.errBack);
+          } else if(window.navigator.webkitGetUserMedia) { // WebKit-prefixed
+                  window.navigator.webkitGetUserMedia($scope.videoObj, function(stream){
+                          $scope.video.src = window.URL.createObjectURL(stream);
+                          $scope.video.play();
+                          $scope.myStream = stream;
+                  }, $scope.errBack);
+          }
+          else if(window.navigator.mozGetUserMedia) { // Firefox-prefixed
+                  window.navigator.mozGetUserMedia($scope.videoObj, function(stream){
+                          $scope.video.src = window.URL.createObjectURL(stream);
+                          $scope.video.play();
+                          $scope.myStream = stream;
+                  }, $scope.errBack);
+          };
+ 
+        }
+        
+        
+        return breturn;
+      };
+      
+
+ 
+
+      
+      
+      
+      
+      
+      $scope.takepic =     function takepic() {
+        console.log('aaaa');
+        if ($scope.myStream) {
+          //console.log(myStream);
+          $scope.context.drawImage($scope.video, 0, 0, 640, 480);
+          // "image/webp" works in Chrome.
+          // Other browsers will fall back to image/png.
+          //document.querySelector('img').src = canvas.toDataURL('image/webp');
+          //TODO jes fix this
+          memoriesFactory.addMemory($scope.thisEvent, $scope.thisGuestId, $scope.canvas.toDataURL('image/webp'),'image','this is my awesome', false);
+          //memoriesFactory.addMemory(eID, gID, thePic,'image','this is my awesome 2', false);
+          //$scope.uploadPic(canvas.toDataURL('image/webp'))
+          //console.log(canvas.toDataURL('image/webp'));
+        }
+      };
+    
+      $scope.uploadFile = function(element) {
+        //console.log('bbbbb',element.files,arguments );
+        var fd = new FormData();
+          //Take the first selected file
+          fd.append("file", element.files[0]);
+          //console.log('ccccc',fd );
+  
+          $scope.theFile =  element.files[0];
+          //scope.progressVisible = false
+          console.log('file is here: ', $scope.theFile);
+  
+          var reader = new FileReader();
+  
+          reader.onload = function(readerEvt) {
+              var binaryString = readerEvt.target.result;
+              $scope.blobFile = "data:"+ $scope.theFile.type + ";base64,"+ btoa(binaryString);
+              document.getElementById('myimg').src = $scope.blobFile;
+              //console.log($scope.blobFile);
+  
+              memoriesFactory.addMemory($scope.thisEvent, $scope.thisGuestId, $scope.blobFile,'image','this is my awesome', false);
+          };
+  
+          reader.readAsBinaryString($scope.theFile);
+  
+      };
+      
+      
   }]);
 
   app.controller('EventsCtrl', ['$scope', '$location','eventsFactory', 'guestFactory', 'memoriesFactory',
